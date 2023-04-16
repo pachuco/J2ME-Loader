b93370145c1f33ad560cb152ee72c8a4045006e4
pachuco <error_man@2die4.com>
2023-04-16 15:48:07 +0300

    Reverse call order, see if that works for midi files.

--- app/src/main/java/javax/microedition/media/MidiInterface.java
+++ app/src/main/java/javax/microedition/media/MidiInterface.java
@@ -25,8 +25,8 @@ public class MidiInterface {
 	public static synchronized MidiDriver getDriver() {
 		if (driver == null) {
 			driver = MidiDriver.getInstance();
-			driver.start();
 			driver.setReverb(ReverbConstants.OFF);
+			driver.start();
 		}
 		return driver;
 	}
